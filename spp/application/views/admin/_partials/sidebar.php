a4443a31c9c721b7a57727cf4d6974b9a95f0cf7
unknown <ramdhaniaza09@gmail.com>
2021-10-21 12:21:07 +0700

    modul 4.0

---
+++ spp/application/views/admin/_partials/sidebar.php
@@ -0,0 +1,37 @@
+<aside class="menu-sidebar d-none d-lg-block">
+            <div class="logo">
+                <a href="#">
+                    <strong>SMKN 4 BOGOR</strong>
+                </a>
+            </div>
+            <div class="menu-sidebar__content js-scrollbar1">
+                <nav class="navbar-sidebar">
+                    <ul class="list-unstyled navbar__list">
+                        <li class="active has-sub">
+                            <a class="js-arrow" href="#">
+                                <i class="fas fa-tachometer-alt"></i>Beranda</a>
+                            <ul class="list-unstyled navbar__sub-list js-sub-list">
+                                
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="chart.html">
+                                <i class="fas fa-chart-bar"></i>SPP</a>
+                        </li>
+                        <li>
+                            <a href="table.html">
+                                <i class="fas fa-table"></i>Kelas</a>
+                        </li>
+                        <li>
+                            <a href="form.html">
+                                <i class="far fa-check-square"></i>Petugas</a>
+                        </li>
+                        <li>
+                            <a href="calendar.html">
+                                <i class="fas fa-calendar-alt"></i>Transaksi Pembayaran</a>
+                        </li>
+                        
+                    </ul>
+                </nav>
+            </div>
+        </aside>
